2b9d5d878fceb1a13be623500d3a6e8f709fef78
WebStreamr <210764791+webstreamr@users.noreply.github.com>
2025-05-16 22:28:17 +0200

    bug(extractor): do not throw for broken/outdated link

--- src/extractor/ExtractorRegistry.test.ts
+++ src/extractor/ExtractorRegistry.test.ts
@@ -9,13 +9,17 @@ const logger = winston.createLogger({ transports: [new winston.transports.Consol
 const fetcher = new Fetcher();
 const extractorRegistry = new ExtractorRegistry(logger, fetcher);
 
-describe('EmbedExtractorRegistry', () => {
+describe('ExtractorRegistry', () => {
   const ctx: Context = { ip: '127.0.0.1' };
 
-  test('returns undefined when no embed extractor can be found', async () => {
+  test('returns undefined when no extractor can be found', async () => {
     const urlResult = await extractorRegistry.handle(ctx, new URL('https://some-url.test'), 'en');
 
     expect(urlResult).toBeUndefined();
+  });
+
+  test('returns undefined when extractor fails', async () => {
+    expect(await extractorRegistry.handle(ctx, new URL('https://mixdrop.my/e/123456789'), 'en')).toBeUndefined();
   });
 
   test('returns from memory cache if possible', async () => {
