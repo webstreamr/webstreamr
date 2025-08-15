9854d3b5b6e01a09d6e10e71f3365fabc1d0c4ef
WebStreamr <210764791+webstreamr@users.noreply.github.com>
2025-08-15 10:32:43 +0000

    test: use proxy in tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,6 @@
+import { socksDispatcher } from 'fetch-socks';
+import { ProxyAgent, setGlobalDispatcher } from 'undici';
+
 jest.mock('randomstring', () => ({
   generate: jest.fn(() => 'mocked-random-string'),
 }));
@@ -5,3 +8,12 @@ jest.mock('randomstring', () => ({
 beforeEach(() => {
   jest.spyOn(Date, 'now').mockImplementation(() => 639837296000);
 });
+
+if (process.env['ALL_PROXY']) {
+  const proxyUrl = new URL(process.env['ALL_PROXY']);
+  if (proxyUrl.protocol === 'socks5:') {
+    setGlobalDispatcher(socksDispatcher({ type: 5, host: proxyUrl.hostname, port: parseInt(proxyUrl.port) }));
+  } else {
+    setGlobalDispatcher(new ProxyAgent({ uri: proxyUrl.href }));
+  }
+}
