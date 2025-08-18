36f3d70467bca8396d0af847b110ab8b6688772e
WebStreamr <210764791+webstreamr@users.noreply.github.com>
2025-08-18 12:30:28 +0000

    chore(extractor): extract size and title for Fastream URLs

--- src/extractor/Fastream.ts
+++ src/extractor/Fastream.ts
@@ -1,5 +1,6 @@
+import bytes from 'bytes';
 import { Context, CountryCode, Format, UrlResult } from '../types';
-import { buildMediaFlowProxyExtractorStreamUrl, Fetcher, guessHeightFromPlaylist, supportsMediaFlowProxy } from '../utils';
+import { buildMediaFlowProxyExtractorStreamUrl, Fetcher, supportsMediaFlowProxy } from '../utils';
 import { Extractor } from './Extractor';
 
 export class Fastream extends Extractor {
@@ -23,8 +24,18 @@ export class Fastream extends Extractor {
     return null !== url.host.match(/fastream/) && supportsMediaFlowProxy(ctx);
   }
 
+  public override normalize(url: URL): URL {
+    return new URL(url.href.replace('/e/', '/embed-').replace('/d/', '/embed-'));
+  }
+
   protected async extractInternal(ctx: Context, url: URL, countryCode: CountryCode): Promise<UrlResult[]> {
     const playlistUrl = await buildMediaFlowProxyExtractorStreamUrl(ctx, this.fetcher, 'Fastream', url);
+
+    const downloadUrl = new URL(url.href.replace('/embed-', '/d/'));
+    const html = await this.fetcher.text(ctx, downloadUrl);
+
+    const heightAndSizeMatch = html.match(/\d{3,}x(\d{3,}), ([\d.]+ ?[GM]B)/) as string[];
+    const titleMatch = html.match(/>Download (.*?)</) as string[];
 
     return [
       {
@@ -35,7 +46,9 @@ export class Fastream extends Extractor {
         ttl: this.ttl,
         meta: {
           countryCodes: [countryCode],
-          height: await guessHeightFromPlaylist(ctx, this.fetcher, playlistUrl),
+          bytes: bytes.parse(heightAndSizeMatch[2] as string) as number,
+          height: parseInt(heightAndSizeMatch[1] as string),
+          title: titleMatch[1],
         },
       },
     ];
