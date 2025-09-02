ccf8006f1368e36a8431e7f2a5f9c768ee612fe2
WebStreamr <210764791+webstreamr@users.noreply.github.com>
2025-09-02 13:40:22 +0000

    chore(extractor): cache Dropload for 3h

--- src/extractor/Dropload.ts
+++ src/extractor/Dropload.ts
@@ -10,6 +10,8 @@ export class Dropload extends Extractor {
 
   public readonly label = 'Dropload';
 
+  public override readonly ttl: number = 10800000; // 3h
+
   public supports(_ctx: Context, url: URL): boolean {
     return null !== url.host.match(/dropload/);
   }
