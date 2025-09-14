4ca87f0ff1789755e412f6bb3151a2c2d76d5121
WebStreamr <210764791+webstreamr@users.noreply.github.com>
2025-09-14 13:37:08 +0000

    revert: Revert "chore(fetcher): log proxy usage, allow to disable proxy via config"
    This reverts commit b67e1b85d634be413e004625b13403acc445ce72.

--- src/utils/dispatcher.ts
+++ src/utils/dispatcher.ts
@@ -3,7 +3,15 @@ import { minimatch } from 'minimatch';
 import { Dispatcher, ProxyAgent } from 'undici';
 import { Context } from '../types';
 
-export const getProxyForUrl = (ctx: Context, url: URL): URL | undefined => {
+const createProxyAgent = (proxyUrl: URL): Dispatcher => {
+  if (proxyUrl.protocol === 'socks5:') {
+    return socksDispatcher({ type: 5, host: proxyUrl.hostname, port: parseInt(proxyUrl.port) });
+  }
+
+  return new ProxyAgent({ uri: proxyUrl.href });
+};
+
+const createBasicDispatcher = (ctx: Context, url: URL): Dispatcher | undefined => {
   const proxyConfig = ctx.config['proxyConfig'] || process.env['PROXY_CONFIG'];
 
   if (proxyConfig) {
@@ -14,20 +22,16 @@ export const getProxyForUrl = (ctx: Context, url: URL): URL | undefined => {
       }
 
       if (hostPattern === '*' || minimatch(url.host, hostPattern)) {
-        return proxy === 'false' ? undefined : new URL(proxy);
+        return createProxyAgent(new URL(proxy));
       }
     }
   } else if (process.env['ALL_PROXY']) {
-    return new URL(process.env['ALL_PROXY']);
+    return createProxyAgent(new URL(process.env['ALL_PROXY']));
   }
 
   return undefined;
 };
 
-export const createProxyAgent = (proxyUrl: URL): Dispatcher => {
-  if (proxyUrl.protocol === 'socks5:') {
-    return socksDispatcher({ type: 5, host: proxyUrl.hostname, port: parseInt(proxyUrl.port) });
-  }
-
-  return new ProxyAgent({ uri: proxyUrl.href });
+export const createDispatcher = (ctx: Context, url: URL): Dispatcher | undefined => {
+  return createBasicDispatcher(ctx, url);
 };
