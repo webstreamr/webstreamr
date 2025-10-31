1d05fe0f98f5c6e6def55ebafeb2e93bef7c9007
WebStreamr <210764791+webstreamr@users.noreply.github.com>
2025-10-31 15:17:44 +0000

    feat(extractor): add Vidora extractor

---
+++ src/extractor/Vidora.ts
@@ -0,0 +1,47 @@
+import * as cheerio from 'cheerio';
+import { Context, Format, Meta, UrlResult } from '../types';
+import { extractUrlFromPacked, guessHeightFromPlaylist } from '../utils';
+import { Extractor } from './Extractor';
+
+export class Vidora extends Extractor {
+  public readonly id = 'vidora';
+
+  public readonly label = 'Vidora';
+
+  public override readonly ttl: number = 10800000; // 3h TODO: verify how long we can cache
+
+  public supports(_ctx: Context, url: URL): boolean {
+    return null !== url.host.match(/vidora/);
+  }
+
+  public override normalize(url: URL): URL {
+    return new URL(url.href.replace('/embed/', '/'));
+  }
+
+  protected async extractInternal(ctx: Context, url: URL, meta: Meta): Promise<UrlResult[]> {
+    const headers = { Origin: new URL(meta.referer ?? url.href).origin };
+
+    const html = await this.fetcher.text(ctx, url, { headers });
+
+    const $ = cheerio.load(html);
+    const title = $('title').text().trim().replace(/^Watch /, '').trim();
+
+    const m3u8Url = extractUrlFromPacked(html, [/file: ?"(.*?)"/]);
+
+    return [
+      {
+        url: m3u8Url,
+        format: Format.hls,
+        label: this.label,
+        sourceId: `${this.id}_${meta.countryCodes?.join('_')}`,
+        ttl: this.ttl,
+        meta: {
+          ...meta,
+          height: await guessHeightFromPlaylist(ctx, this.fetcher, m3u8Url, url, { headers }),
+          title,
+        },
+        requestHeaders: headers,
+      },
+    ];
+  };
+}
