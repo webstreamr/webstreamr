225872c6c898caa2c650cc92a22ce35483492b14
GSTAR <44748406+GLlgGL@users.noreply.github.com>
2025-11-08 09:18:37 +0100

    Update Streamtape.ts

--- src/extractor/Streamtape.ts
+++ src/extractor/Streamtape.ts
@@ -15,29 +15,30 @@ export class Streamtape extends Extractor {
   public supports(ctx: Context, url: URL): boolean {
     const supportedDomain = null !== url.host.match(/streamtape/)
       || [
-      'strtape.cloud',
-      'streamta.pe',
-      'strcloud.link',
-      'strcloud.club',
-      'strtpe.link',
-      'scloud.online',
-      'stape.fun',
-      'streamadblockplus.com',
-      'shavetape.cash',
-      'streamta.site',
-      'streamadblocker.xyz',
-      'tapewithadblock.org',
-      'adblocktape.wiki',
-      'antiadtape.com',
-      'tapeblocker.com',
-      'streamnoads.com',
-      'tapeadvertisement.com',
-      'tapeadsenjoyer.com',
-      'watchadsontape.com',
-    ].includes(url.host);
+        'strtape.cloud',
+        'streamta.pe',
+        'strcloud.link',
+        'strcloud.club',
+        'strtpe.link',
+        'scloud.online',
+        'stape.fun',
+        'streamadblockplus.com',
+        'shavetape.cash',
+        'streamta.site',
+        'streamadblocker.xyz',
+        'tapewithadblock.org',
+        'adblocktape.wiki',
+        'antiadtape.com',
+        'tapeblocker.com',
+        'streamnoads.com',
+        'tapeadvertisement.com',
+        'tapeadsenjoyer.com',
+        'watchadsontape.com',
+      ].includes(url.host);
 
-  return supportedDomain && supportsMediaFlowProxy(ctx);
-    
+    return supportedDomain && supportsMediaFlowProxy(ctx);
+  }
+
   public override normalize(url: URL): URL {
     return new URL(url.href.replace('/e/', '/v/'));
   }
