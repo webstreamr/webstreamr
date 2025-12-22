924265b9640385b33e3a7a8dfb61b4993442e52f
WebStreamr <210764791+webstreamr@users.noreply.github.com>
2025-12-22 10:34:18 +0000

    fix(source): determine Frembed domain dynamically

--- src/source/Frembed.ts
+++ src/source/Frembed.ts
@@ -1,3 +1,4 @@
+import memoize from 'memoizee';
 import { ContentType } from 'stremio-addon-sdk';
 import { Context, CountryCode } from '../types';
 import { Fetcher, getTmdbId, getTmdbNameAndYear, Id } from '../utils';
@@ -12,7 +13,7 @@ export class Frembed extends Source {
 
   public readonly countryCodes: CountryCode[] = [CountryCode.fr];
 
-  public readonly baseUrl = 'https://frembed.my'; // TODO: determine this more dynamically, e.g. via https://audin213.com/
+  public readonly baseUrl = 'https://frembed.my';
 
   private readonly fetcher: Fetcher;
 
@@ -20,17 +21,24 @@ export class Frembed extends Source {
     super();
 
     this.fetcher = fetcher;
+
+    this.getBaseUrl = memoize(this.getBaseUrl, {
+      maxAge: 3600000, // 1 hour
+      normalizer: () => 'baseUrl',
+    });
   }
 
   public async handleInternal(ctx: Context, _type: string, id: Id): Promise<SourceResult[]> {
     const tmdbId = await getTmdbId(ctx, this.fetcher, id);
     const [, year] = await getTmdbNameAndYear(ctx, this.fetcher, tmdbId);
 
-    const apiUrl = tmdbId.season
-      ? new URL(`/api/series?id=${tmdbId.id}&sa=${tmdbId.season}&epi=${tmdbId.episode}&idType=tmdb`, this.baseUrl)
-      : new URL(`/api/films?id=${tmdbId.id}&idType=tmdb`, this.baseUrl);
+    const baseUrl = await this.getBaseUrl(ctx);
 
-    const json = await this.fetcher.json(ctx, apiUrl, { headers: { Referer: this.baseUrl } });
+    const apiUrl = tmdbId.season
+      ? new URL(`/api/series?id=${tmdbId.id}&sa=${tmdbId.season}&epi=${tmdbId.episode}&idType=tmdb`, baseUrl)
+      : new URL(`/api/films?id=${tmdbId.id}&idType=tmdb`, baseUrl);
+
+    const json = await this.fetcher.json(ctx, apiUrl, { headers: { Referer: baseUrl.origin } });
 
     const urls: URL[] = [];
     for (const key in json) {
@@ -47,6 +55,14 @@ export class Frembed extends Source {
       ? `${json['title']} ${tmdbId.formatSeasonAndEpisode()}`
       : `${json['title']} (${year})`;
 
-    return urls.map(url => ({ url, meta: { countryCodes: [CountryCode.fr], referer: this.baseUrl, title } }));
+    return urls.map(url => ({ url, meta: { countryCodes: [CountryCode.fr], referer: baseUrl.origin, title } }));
+  };
+
+  private readonly getBaseUrl = async (ctx: Context): Promise<URL> => {
+    const html = await this.fetcher.text(ctx, new URL('https://audin213.com/'));
+
+    const frembedMatcher = html.match(/"(https:\/\/frembed.*?)"/) as string[];
+
+    return new URL(frembedMatcher[1] as string);
   };
 }
