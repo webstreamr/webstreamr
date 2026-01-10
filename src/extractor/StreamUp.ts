baf5c9143d4dab4607de891f5b17184e89e027c7
GSTAR <44748406+GLlgGL@users.noreply.github.com>
2026-01-10 07:39:19 +0100

    feat(extractor): add StreamUP (#621)
    Co-authored-by: WebStreamr <210764791+webstreamr@users.noreply.github.com>

---
+++ src/extractor/StreamUp.ts
@@ -0,0 +1,49 @@
+import * as cheerio from 'cheerio';
+import { Context, Format, Meta, UrlResult } from '../types';
+import { guessHeightFromPlaylist } from '../utils';
+import { Extractor } from './Extractor';
+
+interface StreamUpApiData {
+  streaming_url: string;
+}
+
+/** @see https://github.com/Gujal00/ResolveURL/blob/master/script.module.resolveurl/lib/resolveurl/plugins/streamup.py */
+export class StreamUp extends Extractor {
+  public readonly id = 'streamup';
+
+  public readonly label = 'StreamUP';
+
+  public override readonly ttl: number = 10800000; // 3h
+
+  public supports(_ctx: Context, url: URL): boolean {
+    return null !== url.host.match(/streamup|strmup/) || [
+      'vfaststream.com',
+    ].includes(url.host);
+  }
+
+  protected async extractInternal(ctx: Context, url: URL, meta: Meta): Promise<UrlResult[]> {
+    const headers = { Referer: `${url.origin}/`, Origin: url.origin };
+
+    const html = await this.fetcher.text(ctx, url, { headers });
+
+    const data = await this.fetcher.json(ctx, new URL(`/ajax/stream?filecode=${url.pathname.split('/').at(-1)}`, url), { headers }) as StreamUpApiData;
+    const playlistUrl = new URL(data.streaming_url);
+
+    const $ = cheerio.load(html);
+    const title = $('title').text().trim();
+
+    return [
+      {
+        url: playlistUrl,
+        format: Format.hls,
+        label: this.label,
+        ttl: this.ttl,
+        meta: {
+          ...meta,
+          height: await guessHeightFromPlaylist(ctx, this.fetcher, playlistUrl, url),
+          title,
+        },
+      },
+    ];
+  }
+}
