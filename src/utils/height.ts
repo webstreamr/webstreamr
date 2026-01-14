a189cddd370c84cc22a93809827c8b988cbbc13f
WebStreamr <210764791+webstreamr@users.noreply.github.com>
2026-01-14 11:51:32 +0000

    chore: do not ignore errors on height guessing

--- src/utils/height.ts
+++ src/utils/height.ts
@@ -2,13 +2,7 @@ import { Context } from '../types';
 import { CustomRequestConfig, Fetcher } from './Fetcher';
 
 export const guessHeightFromPlaylist = async (ctx: Context, fetcher: Fetcher, playlistUrl: URL, init?: CustomRequestConfig): Promise<number | undefined> => {
-  let m3u8Data: string;
-  try {
-    m3u8Data = await fetcher.text(ctx, playlistUrl, init);
-  } catch {
-    /* istanbul ignore next */
-    return undefined;
-  }
+  const m3u8Data = await fetcher.text(ctx, playlistUrl, init);
 
   const heights = Array.from(m3u8Data.matchAll(/\d+x(\d+)|(\d+)p/g))
     .map(heightMatch => heightMatch[1] ?? heightMatch[2])
